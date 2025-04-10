6c0dedeedcfad8288f807575d899421fea766ae2
thisisaaronland <thisisaaronland@localhost>
2025-04-09 18:23:04 -0700

    update to use whosonfirst/go-whosonfirst-iterwriter/v3

--- cmd/riordan/main.go
+++ cmd/riordan/main.go
@@ -3,18 +3,17 @@ package main
 import (
 	"context"
 	"log"
-	"log/slog"
 	
 	_ "github.com/whosonfirst/go-whosonfirst-iterate-organization"
 	_ "github.com/whosonfirst/go-writer-featurecollection/v3"
 	
-	"github.com/whosonfirst/go-whosonfirst-iterwriter/app/iterwriter"
+	"github.com/whosonfirst/go-whosonfirst-iterwriter/v3/app/iterwriter"
 )
 
 func main() {
 
 	ctx := context.Background()
-	err := iterwriter.Run(ctx, slog.Default())
+	err := iterwriter.Run(ctx)
 
 	if err != nil {
 		log.Fatalf("Failed to run iterwriter, %v", err)
